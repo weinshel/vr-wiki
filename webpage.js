b54915b3a170b508a725688e451d0526d3d2e205
Ben Weinshel <ben@weinshel.net>
2019-02-22 10:05:33 -0600

    working page loads, refactoring a bit

--- webpage.js
+++ webpage.js
@@ -1,45 +1,49 @@
-async function getPage () {
+async function getPage (url) {
   
   var xhr = new XMLHttpRequest();
   xhr.onload = function() {
       const el = document.getElementById('htmlElement')
-      const content = this.responseXML.getElementsByTagName('pre')[0]
+      const head = this.responseXML.head
+      const content = this.responseXML.body
+      el.appendChild(head)
       el.appendChild(content)
   }
-  xhr.open("GET", "/https://en.wikipedia.org/wiki/Internet");
+  xhr.open("GET", "/" + url);
   xhr.responseType = "document";
   xhr.send();  
 }
-getPage()  
 
-// AFRAME.registerComponent('webpage', {
-//   schema: {
-//     width: {type: 'number', default: 20},
-//     height: {type: 'number', default: 20},
-//     color: {type: 'color', default: '#AAA'}
-//   },
+AFRAME.registerComponent('webpage', {
+  schema: {
+    width: {type: 'number', default: 20},
+    height: {type: 'number', default: 20},
+    color: {type: 'color', default: '#AAA'},
+    url: {type: 'string', default: 'http://example.com'}
+  },
 
-//   /**
-//    * Initial creation and setting of the mesh.
-//    */
-//   init: async function () {
-//     var data = this.data;
-//     var el = this.el;
+  /**
+   * Initial creation and setting of the mesh.
+   */
+  init: async function () {
+    var data = this.data;
+    var el = this.el;
 
-//     // Create geometry.
-//     this.geometry = new THREE.PlaneGeometry(data.width, data.height);
+    getPage(data.url)  
+
+    // Create geometry.
+    // this.geometry = new THREE.PlaneGeometry(data.width, data.height);
     
-//     // var para = document.createElement("p");
-//     // var node = document.createTextNode("This is new.");
-//     // para.appendChild(node);
-//     // const canvas = await html2canvas(null)
+    // var para = document.createElement("p");
+    // var node = document.createTextNode("This is new.");
+    // para.appendChild(node);
+    // const canvas = await html2canvas(null)
 
-//     // Create mesh.
-//     const texture = new THREE.TextureLoader().load( 'https://cdn.glitch.com/d558c128-2ed0-4284-a0da-4d18b9163ad6%2FGeneric_Celestia_asteroid_texture.jpg?1541189998715' );
-//     this.material = new THREE.MeshBasicMaterial( { map: texture } );
-//     this.mesh = new THREE.Mesh(this.geometry, this.material);
+    // Create mesh.
+    // const texture = new THREE.TextureLoader().load( 'https://cdn.glitch.com/d558c128-2ed0-4284-a0da-4d18b9163ad6%2FGeneric_Celestia_asteroid_texture.jpg?1541189998715' );
+    // this.material = new THREE.MeshBasicMaterial( { map: texture } );
+    // this.mesh = new THREE.Mesh(this.geometry, this.material);
 
-//     // Set mesh on entity.
-//     el.setObject3D('mesh', this.mesh);
-//   }
-// });
+    // Set mesh on entity.
+    // el.setObject3D('mesh', this.mesh);
+  }
+});
